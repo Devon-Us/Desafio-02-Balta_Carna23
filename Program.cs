786fd023a8748eb84537972ffa4de5b3f98c5709
Kelvin Campos <kelvin_campos@ymail.com>
2023-02-18 09:25:52 -0300

    second commit

--- Program.cs
+++ Program.cs
@@ -10,21 +10,42 @@
         var altura = float.Parse(Console.ReadLine());
         var imc = peso / (altura * altura);
         Console.WriteLine($"Seu IMC é: {imc}");
-        if (imc < 18.5f)
+        if (imc < 16)
         {
-            Console.WriteLine("Seu IMC indica baixo peso!");
+            Console.WriteLine("Seu IMC indica Magreza Grau III!");
+        }
+        if (imc > 16 && imc < 16.99f)
+        {
+            Console.WriteLine("Seu IMC indica Magreza Grau II!");
+        }
+        if (imc > 17f && imc < 18.49f)
+        {
+            Console.WriteLine("Seu IMC indica Magreza Grau I!");
         }
         if (imc > 18.5f && imc < 24.99f)
         {
-            Console.WriteLine("Seu IMC indica peso normal!");
+            Console.WriteLine("Seu IMC indica peso Normal(Eutrofia)!");
         }
         if (imc > 25f && imc < 29.99f)
         {
-            Console.WriteLine("Seu IMC indica sobre peso!");
+            Console.WriteLine("Seu IMC indica Sobrepeso!");
+            Console.WriteLine("Risco a saúde: Aumentado");
         }
-        if (imc > 30f)
+        if (imc > 30f && imc < 34.99f)
         {
-            Console.WriteLine("Seu IMC indica obesidade!");
+            Console.WriteLine("Seu IMC indica Obesidade Grau I!");
+            Console.WriteLine("Risco a saúde: Moderado");
         }
+        if (imc > 35f && imc < 40f)
+        {
+            Console.WriteLine("Seu IMC indica Obesidade Grau II!");
+            Console.WriteLine("Risco a saúde: Grave");
+        }
+        if (imc > 40f)
+        {
+            Console.WriteLine("Seu IMC indica Obesidade Grau III!");
+            Console.WriteLine("Risco a saúde: Muito Grave");
+        }
+        Console.WriteLine("Classificações do IMC segundo a OMS(Ordem Mundial da Saude)");
     }
 }
